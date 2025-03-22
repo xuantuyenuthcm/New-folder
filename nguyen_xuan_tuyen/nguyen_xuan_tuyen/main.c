4ea5d6f92ab44416b7517fbc559e68f5bb260ed7
xuantuyenuthcm <nxuantuyen2204@gmail.com>
2025-03-22 14:36:58 +0700

    String_Pointer: Xây dụng hàm đếm số ký tự trong chuỗi

--- nguyen_xuan_tuyen/nguyen_xuan_tuyen/main.c
+++ nguyen_xuan_tuyen/nguyen_xuan_tuyen/main.c
@@ -1,14 +1,14 @@
+﻿
 #include <stdio.h>
-#include "toan.h"
+#include <string.h>
+// Bài 1: Xây dụng hàm đếm số ký tự trong chuỗi
 
-void main()
-{
-	int x = Cong(1, 2);
-	int y = Tru(5, 8);
-	int z = Nhan(4, 7);
-	int w = Chia(8, 2);
-	printf("x : %d\n", x);
-	printf("y : %d\n", y);
-	printf("z : %d\n", z);
-	printf("w : %d\n", w);
+int dem_ki_tu(const char *s) {
+    return strlen(s); // Dùng hàm strlen để đếm số ký tự
+}
+
+int main() {
+    char str[] = "Xuan Tuyen";
+    printf("So ki tu trong chuoi la: %d\n", dem_ki_tu(str));
+    return 0;
 }
